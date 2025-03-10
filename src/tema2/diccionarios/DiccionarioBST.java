67ba11323af6bace7d38f5a24991b032539ba97d
Javier Andion <j.andion@upm.es>
2025-03-10 13:30:13 +0100

    BST recorridos y borrar elementos

--- src/tema2/diccionarios/DiccionarioBST.java
+++ src/tema2/diccionarios/DiccionarioBST.java
@@ -56,7 +56,74 @@ public class DiccionarioBST implements Diccionario {
 
     @Override
     public String remove(String key) {
-        return "";
+        return remove(root, key);
+    }
+
+    private String remove(Nodo nodo, String key) {
+        if (nodo== null) return null;
+        if (nodo.key.equals(key)) {
+            // he encontrado lo que buscaba
+            if (nodo.hijoIzquierdo == null && nodo.hijoDerecho == null) {
+                // lo borro directamente
+
+                // buscamos al padre
+                Nodo padre = root;
+                while(!padre.hijoIzquierdo.equals(nodo) || !padre.hijoDerecho.equals(nodo)) {
+                    if (padre.key.compareTo(key)>0){
+                        padre = padre.hijoIzquierdo;
+                    } else {
+                        padre = padre.hijoDerecho;
+                    }
+                }
+
+                if(padre.hijoIzquierdo.equals(nodo)){
+                    padre.hijoIzquierdo = null;
+                } else {
+                    padre.hijoDerecho = null;
+                }
+                return nodo.value;
+            } else if (nodo.hijoIzquierdo == null || nodo.hijoDerecho == null) {
+                // lo sustituyo por el hijo
+                String temp  = nodo.value;
+                if (nodo.hijoIzquierdo == null) {
+                    nodo.key = nodo.hijoDerecho.key;
+                    nodo.value = nodo.hijoDerecho.value;
+                    nodo.hijoDerecho = null;
+                } else {
+                    nodo.key = nodo.hijoIzquierdo.key;
+                    nodo.value = nodo.hijoIzquierdo.value;
+                    nodo.hijoIzquierdo = null;
+                }
+                return temp;
+            } else {
+                // buscamos el mayor nieto por la izquierda
+                Nodo actual = nodo.hijoIzquierdo;
+                while( actual.hijoDerecho!=null){
+                    actual = actual.hijoDerecho;
+                }
+                // sustuir nodo por actual
+
+                if (actual.hijoIzquierdo == null) {
+                    // lo puedo borrar directamente
+                    // TODO: lo mismo que antes
+                    return null;
+                } else {
+                    // lo sustituyo por el hijo
+                    nodo.key = actual.key;
+                    nodo.value = actual.value;
+                    actual.key = actual.hijoIzquierdo.key;
+                    actual.value = actual.hijoIzquierdo.value;
+                    actual.hijoIzquierdo = null;
+                }
+                return nodo.value;
+            }
+        } else {
+            if (nodo.key.compareTo(key)>0){
+                return remove(nodo.hijoIzquierdo, key);
+            } else {
+                 return remove(nodo.hijoDerecho, key);
+            }
+        }
     }
 
     @Override
@@ -67,5 +134,43 @@ public class DiccionarioBST implements Diccionario {
     private int size(Nodo nodo) {
         if(nodo == null) return 0;
         return 1 + size(nodo.hijoIzquierdo) + size(nodo.hijoDerecho);
+    }
+
+    public int altura(){
+        if (root==null) return 0;
+        return altura(root);
+    }
+
+    private int altura(Nodo nodo) {
+        if(nodo == null) return 0;
+        return 1 + Math.max(altura(nodo.hijoIzquierdo), altura(nodo.hijoDerecho));
+    }
+
+    public void recorridoEnOrden() {
+        recorridoEnOrden(root);
+    }
+    private void recorridoEnOrden(Nodo nodo) {
+        if (nodo == null) return;
+        recorridoEnOrden(nodo.hijoIzquierdo);
+        System.out.println(nodo.key + " "+ nodo.value);
+        recorridoEnOrden(nodo.hijoDerecho);
+    }
+    public void recorridoPreorden() {
+        recorridoPreorden(root);
+    }
+    private void recorridoPreorden(Nodo nodo) {
+        if (nodo == null) return;
+        System.out.println(nodo.key + " "+ nodo.value);
+        recorridoPreorden(nodo.hijoIzquierdo);
+        recorridoPreorden(nodo.hijoDerecho);
+    }
+    public void recorridoPostorden() {
+        recorridoPostorden(root);
+    }
+    private void recorridoPostorden(Nodo nodo) {
+        if (nodo == null) return;
+        recorridoPostorden(nodo.hijoIzquierdo);
+        recorridoPostorden(nodo.hijoDerecho);
+        System.out.println(nodo.key + " "+ nodo.value);
     }
 }
