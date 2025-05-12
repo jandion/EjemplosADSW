4c516974f3600e56564efd4e3199cbaec9df9184
Javier Andion <j.andion@upm.es>
2025-05-12 11:55:30 +0200

    Ejemplo hamburguesas

---
+++ src/tema3/burger/Pedido.java
@@ -0,0 +1,42 @@
+package tema3.burger;
+
+public class Pedido {
+    public static final String HAMBURGUESA = "🍔";
+    public static final String PATATAS = "🍟";
+    public static final int MAX_ITEMS = 4;
+
+
+    public int hamburguesas, hamburguesasRecogidas;
+    public int patatas, patatasRecogidas;
+    public long creacion;
+
+    public static Pedido pedidoAleatorio() {
+        int nHamburguesas = (int) (1 + Math.random()*MAX_ITEMS);
+        int nPatatas = (int) (Math.random()*MAX_ITEMS);
+        return new Pedido(nHamburguesas, nPatatas);
+    }
+
+    public Pedido(int hamburguesas, int patatas) {
+        this.hamburguesas = hamburguesas;
+        this.patatas = patatas;
+        this.creacion = System.currentTimeMillis();
+    }
+
+    public boolean completado() {
+        return hamburguesas == hamburguesasRecogidas && patatas == patatasRecogidas;
+    }
+
+    @Override
+    public String toString() {
+        String res = "";//"----------------\n";
+        res += "Creado hace %3s segs\n".formatted((System.currentTimeMillis()-creacion)/1000);
+        //res += "----------------\n";
+        res += "Hamburguesas: "+ HAMBURGUESA.repeat(hamburguesas) + "\n";
+        res += "Recibidas:    "+ HAMBURGUESA.repeat(hamburguesasRecogidas) + "\n";
+        //res += "----------------\n";
+        res += "Patatas:      "+ PATATAS.repeat(patatas) + "\n";
+        res += "Recibidas:    "+ PATATAS.repeat(patatasRecogidas);
+        //res += "----------------\n";
+        return res;
+    }
+}
