b22c5a64961aa95152624bdc3f670c977698fad9
Javier Andion <yo.com.es@gmail.com>
2026-02-18 12:04:08 +0100

    g21: mergesort

--- src/g21/tema1/Baraja.java
+++ src/g21/tema1/Baraja.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Baraja {
-	
+
 	public static void main(String[] args) {
-		
+
 		List<Carta> baraja = new ArrayList<>();
 
 		Carta unoDia = new Carta(1, Palo.DIAMANTES);
@@ -16,7 +16,7 @@ public class Baraja {
 		baraja.add(unoDia);
 		baraja.add(unoCor);
 		baraja.add(dosPic);
-		
+
 		for(int i = 0; i<5; i++) {
 			int numero = (int)(Math.random()*12+1);
 			double r = Math.random();
@@ -32,9 +32,9 @@ public class Baraja {
 			}
 			baraja.add(new Carta(numero, p));
 		}
-		
+
 		System.out.println(baraja);
-		
+
 		boolean noOrdenado = false;
 		// Aqui vemos si esta ordenada
 		for (int i = 0; i < baraja.size()-1; i++) {
@@ -46,23 +46,23 @@ public class Baraja {
 				noOrdenado = true;
 			}
 		}
-		
+
 		if (noOrdenado) {
 			// ordenar
 			burbuja(baraja);
 			System.out.println("Ordenado!");
 		}
 	}
-	
+
 	private static void swap(int pos1, int pos2, List<Carta> myList) {
 		Carta aux = myList.get(pos1);
 		myList.set(pos1, myList.get(pos2));
 		myList.set(pos2, aux);
 	}
-	
+
 	private static void burbuja(List<Carta> myList) {
 		boolean hayCambio = true;
-		
+
 		while (hayCambio) {
 			hayCambio = false;
 			// hacemos una pasa buscando burburjas desordenadas
@@ -81,7 +81,7 @@ public class Baraja {
 			int min = i;
 			//buscamos el min entre i y el final
 			for(int j = i; j < myList.size(); j++) {
-				
+
 				// si encuentro un elemento menor en j,
 				// actualizo min = j
 				if(myList.get(j).esMenorOIgual(myList.get(min))) {
@@ -91,5 +91,70 @@ public class Baraja {
 			swap(i, min, myList);
 		}
 	}
+
+	public static void insercion(List<Carta> myList) {
+		for (int i = 1; i < myList.size(); i++) {		// repite lo de dentro n-1 veces
+			insert(myList,i,myList.get(i));				// g(n)
+		}
+	}
+
+	public static void insert(List<Carta> myList, int z, Carta v) {
+		int j = z;														// 1
+		while ( j>0 && v.compareTo(myList.get(j-1)) < 0) {				// n veces
+			j--;														// 1
+		}
+		// desplazar todo lo que esté entre j y z, uno hacia la dcha
+		for( int k = z; k > j; k--) {									// n veces
+			myList.set(k, myList.get(k-1));								// 1 + 1
+		}
+
+		myList.set(j, v);												// 1
+	}
+
+
+	public static List<Carta> mergeSort(List<Carta> myList) {
+		if(myList.size() == 1) {
+			return myList;
+		}
+		// dividir
+		List<Carta> izquierda = new ArrayList<Carta>();
+		List<Carta> derecha = new ArrayList<Carta>();
+		for (int i = 0; i < myList.size(); i++) {
+			if (i < myList.size()/2) {
+				izquierda.add(myList.get(i));
+			} else {
+				derecha.add(myList.get(i));
+			}
+		}
+		izquierda = mergeSort(izquierda);
+		derecha = mergeSort(derecha);
+
+		// mezclar
+		List<Carta> resultado = new ArrayList<Carta>();
+
+		while(izquierda.size()>0 && derecha.size()>0) {
+			if (izquierda.get(0).compareTo(derecha.get(0))<0) {
+				Carta c = izquierda.remove(0);
+				resultado.add(c);
+			} else {
+				resultado.add(derecha.remove(0));
+			}
+		}
+		resultado.addAll(izquierda);
+		resultado.addAll(derecha);
+		
+		return resultado;
+	}
+
+
+
+
+
+
+
+
+
+
+
 
 }
